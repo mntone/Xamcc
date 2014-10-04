d8329afce73f7a29f11046bf036e8e6085f2a5c8
mntone <sd8@live.jp>
2014-10-04 10:03:53 +0900

    Change BooleanToVisibilityConveter.

--- Mntone.Xamcc/Mntone.Xamcc.Shared/Converters/BooleanToVisibilityConverter.cpp
+++ Mntone.Xamcc/Mntone.Xamcc.Shared/Converters/BooleanToVisibilityConverter.cpp
@@ -7,26 +7,23 @@ using namespace Windows::UI::Xaml::Interop;
 using namespace Mntone::Xamcc::Converters;
 
 BooleanToVisibilityConverter::BooleanToVisibilityConverter()
+	: DefaultValue_( true )
 { }
 
 Object^ BooleanToVisibilityConverter::Convert( Object^ value, TypeName /*targetType*/, Object^ /*parameter*/, String^ /*language*/ )
 {
 	auto flag = dynamic_cast<IBox<bool>^>( value );
-	if( flag != nullptr )
+	if( flag )
 	{
 		return flag->Value ^ IsInversed_ ? Visibility::Visible : Visibility::Collapsed;
 	}
-	if( DefaultValue_ != nullptr )
-	{
-		return DefaultValue_->Value ^ IsInversed_ ? Visibility::Visible : Visibility::Collapsed;
-	}
-	return nullptr;
+	return DefaultValue_ ^ IsInversed_ ? Visibility::Visible : Visibility::Collapsed;
 }
 
 Object^ BooleanToVisibilityConverter::ConvertBack( Object^ value, TypeName /*targetType*/, Object^ /*parameter*/, String^ /*language*/ )
 {
 	auto visibility = dynamic_cast<IBox<Visibility>^>( value );
-	if( visibility != nullptr )
+	if( visibility )
 	{
 		return ( visibility->Value == Visibility::Visible ) ^ IsInversed_ ? true : false;
 	}
--- Mntone.Xamcc/Mntone.Xamcc.Shared/Converters/BooleanToVisibilityConverter.h
+++ Mntone.Xamcc/Mntone.Xamcc.Shared/Converters/BooleanToVisibilityConverter.h
@@ -4,22 +4,22 @@ namespace Mntone { namespace Xamcc { namespace Converters {
 
 	[Windows::Foundation::Metadata::WebHostHidden]
 	public ref class BooleanToVisibilityConverter sealed
-		: public Windows::UI::Xaml::Data::IValueConverter
+		: public ::Windows::UI::Xaml::Data::IValueConverter
 	{
 	public:
 		BooleanToVisibilityConverter();
 
-		virtual Platform::Object^ Convert(
-			Platform::Object^ value,
-			Windows::UI::Xaml::Interop::TypeName targetType,
-			Platform::Object^ parameter,
-			Platform::String^ language );
+		virtual ::Platform::Object^ Convert(
+			::Platform::Object^ value,
+			::Windows::UI::Xaml::Interop::TypeName targetType,
+			::Platform::Object^ parameter,
+			::Platform::String^ language );
 
-		virtual Platform::Object^ ConvertBack(
-			Platform::Object^ value,
-			Windows::UI::Xaml::Interop::TypeName targetType,
-			Platform::Object^ parameter,
-			Platform::String^ language );
+		virtual ::Platform::Object^ ConvertBack(
+			::Platform::Object^ value,
+			::Windows::UI::Xaml::Interop::TypeName targetType,
+			::Platform::Object^ parameter,
+			::Platform::String^ language );
 
 	public:
 		property bool IsInversed
@@ -27,15 +27,14 @@ namespace Mntone { namespace Xamcc { namespace Converters {
 			bool get() { return IsInversed_; }
 			void set( bool value ) { IsInversed_ = value; }
 		}
-		property ::Platform::IBox<bool>^ DefaultValue
+		property bool DefaultValue
 		{
-			::Platform::IBox<bool>^ get() { return DefaultValue_; }
-			void set( ::Platform::IBox<bool>^ value ) { DefaultValue_ = value; }
+			bool get() { return DefaultValue_; }
+			void set( bool value ) { DefaultValue_ = value; }
 		}
 
 	private:
-		bool IsInversed_;
-		::Platform::IBox<bool>^ DefaultValue_;
+		bool IsInversed_, DefaultValue_;
 	};
 
 } } }
