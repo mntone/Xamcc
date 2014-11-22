2fe565cfbfb4c5d2d2de2f780519564c15627684
mntone <sd8@live.jp>
2014-11-22 09:24:56 +0900

    Update auto compile file.

--- compile.bat
+++ compile.bat
@@ -1,22 +1,25 @@
 @echo off
 set MSBUILD=msbuild
-set NUGET=nuget
 
 set PROJECT_FILES=.\Mntone.Xamcc\Mntone.Xamcc.Windows\Mntone.Xamcc.Windows.vcxproj .\Mntone.Xamcc\Mntone.Xamcc.WindowsPhone\Mntone.Xamcc.WindowsPhone.vcxproj .\Mntone.Xamcc.Interactions\Mntone.Xamcc.Interactions.Windows\Mntone.Xamcc.Interactions.Windows.vcxproj .\Mntone.Xamcc.Interactions\Mntone.Xamcc.Interactions.WindowsPhone\Mntone.Xamcc.Interactions.WindowsPhone.vcxproj
 
 set PLATFORM_WIN32=Win32
 set PLATFORM_AMD64=x64
 set PLATFORM_ARM=ARM
+set TARGET_PLATFORMS=%PLATFORM_WIN32% %PLATFORM_AMD64% %PLATFORM_ARM%
 
 set CONFIGURATION_DEBUG=Debug
 set CONFIGURATION_RELEASE=Release
+set TARGET_CONFIGURATIONS=%CONFIGURATION_RELEASE%
 
-for %%i in (%PROJECT_FILES%) do %MSBUILD% "%%i" /maxcpucount /t:Clean;Build /p:Configuration=%CONFIGURATION_RELEASE%;Platform=%PLATFORM_WIN32%;GenerateProjectSpecificOutputFolder=false
-for %%i in (%PROJECT_FILES%) do %MSBUILD% "%%i" /maxcpucount /t:Clean;Build /p:Configuration=%CONFIGURATION_RELEASE%;Platform=%PLATFORM_AMD64%;GenerateProjectSpecificOutputFolder=false
-for %%i in (%PROJECT_FILES%) do %MSBUILD% "%%i" /maxcpucount /t:Clean;Build /p:Configuration=%CONFIGURATION_RELEASE%;Platform=%PLATFORM_ARM%;GenerateProjectSpecificOutputFolder=false
+for %%c in (%TARGET_CONFIGURATIONS%) do for %%p in (%TARGET_PLATFORMS%) do for %%f in (%PROJECT_FILES%) do (
+	echo # -----------------------
+	echo # Compile "%%f" in %%p-%%c
+	echo # -----------------------
+	%MSBUILD% "%%f" /maxcpucount /t:Clean;Build /p:Configuration=%%c;Platform=%%p;GenerateProjectSpecificOutputFolder=false
+)
 
 cd ./nuget
-%NUGET% pack Xamcc.nuspec
-%NUGET% pack Xamcc.Interactions.nuspec
+nuget-pack.bat
 
 PAUSE
--- nuget/nuget-pack.bat
+++ nuget/nuget-pack.bat
@@ -1,4 +1,7 @@
 @echo off
+
+set NUGET=nuget
+
 %NUGET% pack Xamcc.nuspec
 %NUGET% pack Xamcc.Interactions.nuspec
 
